96c788ea81862df50a40ceb3c9d87cd9da14379c
Shaurya Dahiya <shauryadahiya@cvent.com>
2020-05-02 18:34:25 +0530

    Updates

---
+++ precache-manifest.bd1176e479c3739cf4ba7f2ff4b3713c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e81d5417004535506b299916b9a191",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "0b16456ab95a34a2bc76",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b16456ab95a34a2bc76",
+    "url": "/robofriends/static/js/main.155c7910.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
